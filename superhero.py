c583ebaddfcdabcb34869611635fed0277d25668
Ben Simpson <b.albertsimpson@gmail.com>
2019-12-02 21:02:52 -0800

    added functionality to Hero class fighting opponent

--- superhero.py
+++ superhero.py
@@ -46,15 +46,56 @@ class Hero:
             return total_damage
 
     def add_armor(self, armor):
+        #adds armor for our characters
         self.armor = armor
         self.armors.append(armor)
 
-    #def defend(self, damage_amt):
+    def defend(self, damage_amt):
+        #Takes in what the damage does to the character
+        blocks = 0
+        self.damage_amt = damage_amt
+        for blocks in self.armors:
+            blocks += damage_amt.block()
+            return blocks
+
+    def take_damage(self, damage):
+        #Confirms the amount of damage taken
+        self.current_health -= damage
+
+    def is_alive(self):
+        #Checks if the Hero has health
+        if self.current_health > 0:
+            return True
+        else:
+            return False
+
+    def fight(self, opponent):
+        #Logic that checks if the hero and opponent are still standing
+        self.opponent = opponent
+        while self.is_alive() and self.opponent.is_alive():
+            if len(self.abilities) > 0 and len(self.opponent.abilities) == 0:
+                self.opponent.take_damage(self.attack())
+            elif len(self.opponent.abilities) > 0 and len(self.abilities) == 0:
+                self.take_damage(self.opponent.attack())
+            else:
+                self.take_damage(self.opponent.attack())
+                self.opponent.take_damage(self.attack())
+        #Checks to see who is the last person standing in the arguement
+        if self.is_alive():
+            print(f"{self.name} won!")
+            
+        else:
+            print(f"{self.opponent.name} won!")
 
 if __name__ == "__main__":
-    ability = Ability("Great Debugging", 50)
-    another_ability = Ability("Smarty Pants", 90)
-    hero = Hero("Grace Hopper", 200)
-    hero.add_ability(ability)
-    hero.add_ability(another_ability)
-    print(hero.attack())
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    hero1.fight(hero2)
